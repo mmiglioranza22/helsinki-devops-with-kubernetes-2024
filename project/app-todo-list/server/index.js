941557b6500f9c28fa4558539db2c01c774522a2
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-09-04 15:22:51 -0300

    init project

---
+++ project/app-todo-list/server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening to port: ${PORT}`);
+});
