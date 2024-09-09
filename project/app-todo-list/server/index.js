ad60f645d4feaf2b9284757b65b9cef418fd4e50
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-09-09 10:53:01 -0300

    1.12 works, surprisingly

--- project/app-todo-list/server/index.js
+++ project/app-todo-list/server/index.js
@@ -5,9 +5,10 @@ const cors = require("cors");
 const morgan = require("morgan");
 const axios = require("axios");
 
-// fetch/save image
+const PORT = process.env.PORT || 5000;
 
-const directory = path.join("/", "usr", "src", "app", "files");
+// get the file or image
+const directory = path.join("/", "usr", "src", "app", "build");
 const filePath = path.join(directory, "image.jpg");
 
 const fileAlreadyExists = async () =>
@@ -18,37 +19,38 @@ const fileAlreadyExists = async () =>
     });
   });
 
+const writableStream = fs.createWriteStream(filePath);
+
+writableStream.on("error", (error) => {
+  console.log(
+    `An error occured while writing to the file. Error: ${error.message}`
+  );
+});
+
 const findAFile = async () => {
   if (await fileAlreadyExists()) return;
+  console.log("Fetching image...");
 
   await new Promise((res) => fs.mkdir(directory, (err) => res()));
   const response = await axios.get("https://picsum.photos/200", {
     responseType: "stream",
   });
-  response.data.pipe(fs.createWriteStream(filePath));
+  console.log("Image fetched!");
+  response.data.pipe(writableStream);
 };
 
 const removeFile = async () =>
   new Promise((res) => fs.unlink(filePath, (err) => res()));
-//
-
+// fetch/save image
 const app = express();
 
-const PORT = process.env.PORT || 8080;
-
 app.use(cors());
+app.use(express.static("./build"));
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", async (req, res) => {
-  try {
-    await findAFile();
-  } catch (error) {
-    console.log("FAILED TO READ FILE", "----------------", error);
-  } finally {
-    res.sendFile(path.join(__dirname, "hello.html"));
-  }
-});
+findAFile();
+app.get("/ping", (req, res) => res.send("pong"));
 
 app.listen(PORT, () => {
   console.log(`Server listening to port: ${PORT}`);
